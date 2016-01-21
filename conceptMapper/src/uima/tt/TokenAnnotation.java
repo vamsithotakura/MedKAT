a827ad5f8f33c704332406d474eb7c90cdba516c
Vamsi Thotakura <vamsi.thotakura666@gmail.com>
2016-01-21 17:01:21 -0500

    conceptMapper

---
+++ conceptMapper/src/uima/tt/TokenAnnotation.java
@@ -0,0 +1,62 @@
+
+
+/* First created by JCasGen Thu Jun 12 14:27:06 EDT 2008 */
+package uima.tt;
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Thu Mar 12 16:28:44 EDT 2009
+ * XML source: C:/eclipse/conceptMapper/src/org/ohnlp/medkat/conceptMapper/support/tokenizer/TokenAnnotation.xml
+ * @generated */
+public class TokenAnnotation extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  public final static int typeIndexID = JCasRegistry.register(TokenAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  public final static int type = typeIndexID;
+  /** @generated  */
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected TokenAnnotation() {}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public TokenAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public TokenAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated */  
+  public TokenAnnotation(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {}
+     
+}
+
+    
